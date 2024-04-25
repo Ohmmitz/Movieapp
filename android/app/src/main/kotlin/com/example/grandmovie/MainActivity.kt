ed7f5069fc3382feb267f662163ba62287986d4b
Thada Phumphung <ohmmitz@OHMMac01.local>
2024-04-25 22:05:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/grandmovie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.grandmovie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:grandmovie/movie.dart';
+
+void main() {
+  runApp(CinemaApp());
+}
